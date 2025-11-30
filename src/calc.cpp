84b7c6f18640e33586287581f393fd846955add3
Anton Pavlenko <anton.pavlenko.85@gmail.com>
2025-11-30 14:36:49 +0000

    Update calculator

---
+++ src/calc.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> // for std::atof , std::atoi
+#include <cmath>
+#include <math_utils/math_utils.hpp>
+
+struct Number
+{
+    int first;
+    int second;
+    std::string operation;
+    long long result;
+};
+
+void print_help()
+{
+    std::cout << "Usage: ./calculator <num1> <operation> <num2>\n";
+    std::cout << "Avaliable operations:\n";
+    std::cout << " add	(+)\n";
+    std::cout << " sub	(-)\n";
+    std::cout << " mul	(*)\n";
+    std::cout << " div	(/)\n";
+    std::cout << " pow 	(^)\n";
+    std::cout << " fact	(!)\n\n";
+    std::cout << "Example:\n";
+    std::cout << " ./calculator 5 add 3\n";
+    std::cout << " ./calculator 10 div 2\n";
+    std::cout << " ./calculator 2 pow 8\n";
+    std::cout << " ./calculator 5 fact 0\n";
+}
+
+void parse(int argc, char *argv[], Number &num)
+{
+    // Arguments sheck
+    if (argc < 4)
+    {
+        if (argc == 3 && (std::string(argv[2]) == "fact" || std::string(argv[2]) == "!"))
+        {
+            // OK call for factorial
+        }
+        else
+        {
+            print_help();
+            return;
+        }
+    }
+
+    // Parsing
+    num.first = std::atoi(argv[1]);
+    num.operation = argv[2];
+    num.second = (argc >= 4) ? std::atoi(argv[3]) : 0.0; // for factorial second argument doesn't matter
+}
+
+void calc(Number &num)
+{
+    if (num.operation == "add" || num.operation == "+")
+    {
+        num.result = math_utils::add(num.first, num.second);
+    }
+    else if (num.operation == "sub" || num.operation == "-")
+    {
+        num.result = math_utils::subtract(num.first, num.second);
+    }
+    else if (num.operation == "mul" || num.operation == "*")
+    {
+        num.result = math_utils::multiply(num.first, num.second);
+    }
+    else if (num.operation == "div" || num.operation == "/")
+    {
+        num.result = math_utils::divide(num.first, num.second);
+    }
+    else if (num.operation == "pow" || num.operation == "^")
+    {
+        num.result = math_utils::power(num.first, num.second);
+    }
+    else if (num.operation == "fact" || num.operation == "!")
+    {
+        num.result = math_utils::factorial(num.first);
+    }
+    else
+    {
+        if (num.operation != "0") // prevention of initial two-time print_help()
+        {
+            std::cout << "Error: unknown operation " << num.operation << ".\n";
+            print_help();
+            return;
+        }
+    }
+}
+
+void printResult(Number &num)
+{
+    std::cout << "Result = " << num.result << std::endl;
+}
+
+void run(int argc, char *argv[])
+{
+    Number num = {0, 0, "0", 0};
+    parse(argc, argv, num);
+    calc(num);
+    printResult(num);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,10 @@
 #include <iostream>
-#include <string>
-#include <cstdlib> // for std::atof , std::atoi
-#include <cmath>
-#include "../math_lib/math_lib.h"
 
-void print_help() {
-	std::cout << "Usage: ./calculator <num1> <operation> <num2>\n";
-	std::cout << "Avaliable operations:\n";
-	std::cout << " add	(+)\n";
-	std::cout << " sub	(-)\n";
-	std::cout << " mul	(*)\n";
-	std::cout << " div	(/)\n";
-	std::cout << " pow 	(^)\n";
-	std::cout << " fact	(!)\n\n";
-	std::cout << "Example:\n";
-	std::cout << " ./calculator 5 add 3\n";
-	std::cout << " ./calculator 10 div 2\n";
-	std::cout << " ./calculator 2 pow 8\n";
-	std::cout << " ./calculator 5 fact 0\n";
-}
+void run(int argc, char *argv[]);
 
-int main (int argc, char* argv[]) {
-	if(argc<4) {
-		if(argc == 3 && (std::string(argv[2]) == "fact" || std::string(argv[2]) == "!")) {
-		// OK call for factorial
-		} else {
-			print_help();
-			return 1;
-		}
-	}
-
-	// Parsing arguments
-	double num1 = std::atof(argv[1]);
-	std::string operation = argv[2];
-	double num2 = (argc >= 4) ? std::atof(argv[3]) : 0.0; // for factorial second argument doesn't matter
-
-	double result_double = 0.0;
-	long long result_long_long = 0;
-	bool is_double_result = true;
-
-	// Executing operation
-	if(operation == "add" || operation == "+") {
-		result_double = add(num1,num2);
-	} else if (operation == "sub" || operation == "-") {
-		result_double = subtract(num1,num2);
-	} else if (operation == "mul" || operation == "*") {
-		result_double = multiply(num1,num2);
-	} else if (operation == "div" || operation == "/") {
-		result_double = divide(num1,num2);
-		if (std::isnan(result_double)) {
-			std::cout << "Error: dision by zero!" << std::endl;
-			return 1;
-		}
-	} else if (operation == "pow" || operation == "^")  {
-		// for power second argument must be int
-		int exponent = static_cast<int>(num2);
-		result_double = power(num1, exponent);
-	} else if (operation == "fact" || operation == "!") {
-		int n = static_cast<int>(num1);
-		result_long_long = factorial(n);
-		is_double_result = false;
-		if (result_long_long == -1) {
-			std::cout << "Error: factorial olny positive integers!" << std::endl;
-			return 1;
-		}
-	} else {
-		std::cout << "Error: unknown operation " << operation << ".\n";
-		print_help();
-		return 1;
-	}
-
-	// Result out
-	if(is_double_result) {
-		std::cout << "Result: " << result_double << std::endl;
-	} else {
-		std::cout << "Result: " << result_long_long << std::endl;
-	}
+int main(int argc, char *argv[])
+{
+	run(argc, argv);
 
 	return 0;
 }
